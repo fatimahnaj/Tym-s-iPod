0bf8ca9e658929a1f8e368b950bdaaea32970559
Fatimah Najihah <fatimahnajihah05@gmail.com>
2025-08-20 20:34:03 +0800

    Added other songs with the MUSIC class

--- main.py
+++ main.py
@@ -109,6 +109,27 @@ Meet_You_Halfway = MUSIC('playlist_1/Meet You Halfway.mp3')
 Moments_of_Good_Time = MUSIC('playlist_1/Moments of Good Time.mp3')
 StudyJazz_playlist = [A_Better_Tomorrow, Amazing_Things, Meet_You_Halfway, Moments_of_Good_Time]
 
+If_its_not_you = MUSIC('playlist_2/if its not you.mp3')
+penjaga_hati = MUSIC('playlist_2/penjaga hati.mp3')
+Take_My_Half = MUSIC('playlist_2/Take My Half.mp3')
+The_Only_Exception = MUSIC('playlist_2/The Only Exception.mp3')
+Youre_here_thats_the_thing = MUSIC('playlist_2/Youre here thats the thing.mp3')
+
+Its_Not_The_Same_Anymore = MUSIC('playlist_3/Its Not The Same Anymore.mp3')
+Kembali_Pulang = MUSIC('playlist_3/Kembali Pulang.mp3')
+No_One_Noticed = MUSIC('playlist_3/No One Noticed.mp3')
+Runtuh = MUSIC('playlist_3/Runtuh.mp3')
+
+Departure = MUSIC('playlist_4/Departure.mp3')
+late_hrs = MUSIC('playlist_4/late hrs.mp3')
+No_Surprises = MUSIC('playlist_4/No Surprises.mp3')
+Skybeam = MUSIC('playlist_4/Skybeam.mp3')
+Space_Things = MUSIC('playlist_4/Space Things.mp3')
+
+SLAY = MUSIC('playlist_5/SLAY!.mp3')
+They_Think_Im_Hiding = MUSIC('playlist_5/They Think Im Hiding in the Shadows... but I Am the Shadows.mp3')
+You_Monster = MUSIC('playlist_5/You Will Always Be a Monster (Slowed)')
+
 #queue
 currently_playing = [A_Better_Tomorrow]
 
@@ -221,6 +242,7 @@ while run:
                         playlist1_screen()
                         page = "playlist_1"
                         print("Changing to screen " + page + " ...")
+                #play selected song from playlist_1
                 elif page == "playlist_1":
                     if current_p1_song == 1:
                         play_new_song(A_Better_Tomorrow)
@@ -233,6 +255,7 @@ while run:
 
             #pause/unpause the song
             elif event.key == pygame.K_SPACE:
+                #if a song is playing, we will pause. else,(song is pause. therefore,) we will unpause.
                 if playing:
                     currently_playing[0].pause_music()
                 else:
